d72cbafe63eac80af72efe5c67aebf8ea5315e36
Juxhin Bazelli <juxhin.bazelli@menatnet.local>
2020-10-23 13:25:35 +0200

    Corrected the readme file in the setup file

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="texttospeech",
     packages=["application"],
     description=["A simple package"],
-    long_description=open("README.txt").read(),
+    long_description=open("README.md").read(),
     install_requires=["pyttsx3", "trafilatura"]
 )
