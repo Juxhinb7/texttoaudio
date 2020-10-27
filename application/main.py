ddf66dcfce0dc1f53880e6358769b2cb7ee7f526
Juxhin Bazelli <juxhin.bazelli@menatnet.local>
2020-10-27 15:49:42 +0100

    Updated the files

--- application/main.py
+++ application/main.py
@@ -1,6 +1,7 @@
 import pyttsx3
 import sys
 import trafilatura
+import os 
 
 
 
@@ -23,22 +24,31 @@ try:
         print(url_text)
         subcategory_input = input("Choose one subcategory. Note! Words that contain whitespaces need to be written with an underscore('_'): ")
         subcategory_fetched = trafilatura.fetch_url("https://de.wikibooks.org/wiki/{}/_{}".format(user_input, subcategory_input))
+        sub_fetched_alternative = trafilatura.fetch_url("https://de.wikibooks.org/wiki/{}:_{}".format(user_input, subcategory_input))
         subcategory_text = trafilatura.extract(subcategory_fetched)
+
+        if (subcategory_text is None):
+            subcategory_text = trafilatura.extract(sub_fetched_alternative)
+            print(subcategory_text)
+         
         to_save_or_not_to_save = input("Do you wish to save the text as audio? (y/n):")
         if (to_save_or_not_to_save == "y"):
+            if not os.path.exists(sys.argv[2]):
+                os.mkdir(sys.argv[2])
+            
             audio_format = input("Choose a format(mp3, wav): ")
             if (audio_format == "mp3"):
-                app.save_to_file(subcategory_text, "{}.{}".format(subcategory_input, audio_format))
+                app.save_to_file(subcategory_text, "{}/{}.{}".format(sys.argv[2], subcategory_input, audio_format))
             elif(audio_format == "wav"):
-                app.save_to_file(subcategory_text, "{}.{}".format(subcategory_input, audio_format))
+                app.save_to_file(subcategory_text, "{}/{}.{}".format(sys.argv[2], subcategory_input, audio_format))
             else:
-                raise ValueError
-        
+                raise ValueError        
         
         
 
 except Exception as e:
     print(e)
+    
     
 
 app.runAndWait()
